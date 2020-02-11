64dafaf5473368539a54297b7e15748b88e3ce22
David Onuh <Youngstizy@gmail.com>
2020-02-12 00:17:25 +0100

    modularize settings file

--- main/settings/common.py
+++ main/settings/common.py
@@ -22,10 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'h@!_&s8y8%qk6q)45@h*y^r$xv%ngqg&zj0fdhslxj--rt+www'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -70,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
---
+++ main/settings/production.py
@@ -0,0 +1,3 @@
+from .common import *
+
+SECRET_KEY = '-yc%8w1^++$jd=%#6(un6!&bpvchzor703c6h3=h-dtpz$*aml'
