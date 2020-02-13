09b7d2a487d1b263de3d7e0dfa8a57095557ed9b
David Onuh <Youngstizy@gmail.com>
2020-02-13 21:58:44 +0100

    add posts and draft models. add users, blog app to settings

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
+from users.models import UserProfile
 # Create your models here.
+
+
+class Posts(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(blank=False, max_length=120)
+    body = models.TextField()
+    author = models.ForeignKey(UserProfile, on_delete= models.CASCADE) 
+    date_created = models.DateTimeField(default=timezone.now)
+    #tags =
+"""
+Subject to removal still don't know why i need a draft
+"""
+class Draft(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(blank=False, max_length=120)
+    body = models.TextField()
+    author = models.ForeignKey(UserProfile, on_delete= models.CASCADE)
+    post_id = models.ForeignKey(Posts, on_delete= models.CASCADE)
+    #tags = 
