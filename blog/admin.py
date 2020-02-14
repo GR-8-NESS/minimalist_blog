04a59e8e7847d54051132cc7a569c035bbbd86fa
David Onuh <Youngstizy@gmail.com>
2020-02-14 11:41:51 +0100

    admin registration for blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
+from .models import Draft, Post
 # Register your models here.
+
+@admin.register(Draft)
+class DraftAdmin(ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
